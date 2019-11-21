035c930c87cf6f19a7a31c420273cb93b60d9a06
Raphael <raphael.sonabend.15@ucl.ac.uk>
2019-11-21 15:25:07 +0000

    docs

--- man-roxygen/SetWrapper.R
+++ man-roxygen/SetWrapper.R
@@ -77,10 +77,11 @@
 #'
 #' @details
 #' The purpose of this class is to provide a symbolic representation for the <%=operation%> of sets that
-#' cannot be represented in a simpler class. Whilst this is not an abstract class, it is not recommended to construct this class directly but via
-#' the set operation methods: [union], [product], [power], [setdiff].
+#' cannot be represented in a simpler class. Whilst this is not an abstract class, it is not recommended to construct
+#' this class directly but via the set operation methods.
 #'
 #' @family wrappers
+#' @seealso [setunion], [setproduct], [power], [setcomplement],  [powerset], [setintersect], [symdiff]
 #'
 #' @return R6 object of class <%=(class)%>, inheriting from SetWrapper.
 #'
